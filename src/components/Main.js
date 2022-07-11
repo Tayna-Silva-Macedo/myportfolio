e76397aaed7c6bafb243157adcdb5fb50b1157e9
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-07-11 20:59:17 -0300

    Main finalizado

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,35 @@ import React from "react";
 
 function Main() {
   return (
-    <main className="container">Main</main>
+    <main className="container">
+      <div className="card-container">
+        <div className="card">
+          <div className="card-wrapper">
+            <h2>Projeto 1</h2>
+            <p>Vem ver!</p>
+          </div>
+        </div>
+        <div className="card-text">Textão aqui</div>
+      </div>
+      <div className="card-container">
+        <div className="card">
+          <div className="card-wrapper">
+            <h2>Projeto 1</h2>
+            <p>Vem ver!</p>
+          </div>
+        </div>
+        <div className="card-text">Textão aqui</div>
+      </div>
+      <div className="card-container">
+        <div className="card">
+          <div className="card-wrapper">
+            <h2>Projeto 1</h2>
+            <p>Vem ver!</p>
+          </div>
+        </div>
+        <div className="card-text">Textão aqui</div>
+      </div>
+    </main>
   )
 }
 
